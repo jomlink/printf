61989ea37b5c1bc53b7be2be6341ea6589d7cb32
Jonas Mwansa <jonasmwansa7@gmail.com>
2022-08-09 23:09:04 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,49 +1,97 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
 
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/* SIZES */
+#define S_LONG 2
+#define S_SHORT 1
+
+/**
+ * struct fmt - Struct op
+ *
+ * @fmt: The format.
+ * @fn: The function associated.
+ */
+struct fmt
+{
+	char fmt;
+	int (*fn)(va_list, char[], int, int, int, int);
+};
 
 
 /**
+ * typedef struct fmt fmt_t - Struct op
  *
- *  * get_size - Calculates the size to cast the argument
- *
- *   * @format: Formatted string in which to print the arguments
- *
- *    * @i: List of arguments to be printed.
- *
- *     *
- *
- *      * Return: Precision.
- *
- *       */
+ * @fmt: The format.
+ * @fm_t: The function associated.
+ */
+typedef struct fmt fmt_t;
 
-int get_size(const char *format, int *i)
+int _printf(const char *format, ...);
+int handle_print(const char *fmt, int *i,
+va_list list, char buffer[], int flags, int width, int precision, int size);
 
-{
+/****************** FUNCTIONS ******************/
 
-		int curr_i = *i + 1;
+/* Funtions to print chars and strings */
+int print_char(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_percent(va_list types, char buffer[], int flags, int width, int precision, int size);
 
-			int size = 0;
+/* Functions to print numbers */
+int print_int(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_binary(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_unsigned(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_octal(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_hexadecimal(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_hexa_upper(va_list types, char buffer[], int flags, int width, int precision, int size);
 
+int print_hexa(va_list types, char map_to[], char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
+/* Function to print non printable characters */
+int print_non_printable(va_list types, char buffer[], int flags, int width, int precision, int size);
 
-				if (format[curr_i] == 'l')
+/* Funcion to print memory address */
+int print_pointer(va_list types, char buffer[],	int flags, int width, int precision, int size);
 
-							size = S_LONG;
+/* Funciotns to handle other specifiers */
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
 
-					else if (format[curr_i] == 'h')
+/*Function to print string in reverse*/
+int print_reverse(va_list types, char buffer[], int flags, int width, int precision, int size);
 
-								size = S_SHORT;
+/*Function to print a string in rot 13*/
+int print_rot13string(va_list types, char buffer[], int flags, int width, int precision, int size);
 
+/* width handler */
+int handle_write_char(char c, char buffer[], int flags, int width, int precision, int size);
+int write_number(int is_positive, int ind, char buffer[], int flags, int width, int precision, int size);
+int write_num(int ind, char bff[], int flags, int width, int precision, int length, char padd, char extra_c);
+int write_pointer(char buffer[], int ind, int length, int width, int flags, char padd, char extra_c, int padd_start);
 
+int write_unsgnd(int is_negative, int ind, char buffer[], int flags, int width, int precision, int size);
 
-			if (size == 0)
+/****************** UTILS ******************/
+int is_printable(char);
+int append_hexa_code(char, char[], int);
+int is_digit(char);
 
-				*i = curr_i - 1;
+long int convert_size_number(long int num, int size);
+long int convert_size_unsgnd(unsigned long int num, int size);
 
-			else
-
-			*i = curr_i;
-
-
-	return (size);
-}
+#endif /* MAIN_H */
